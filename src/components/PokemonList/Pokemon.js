6343c315c015481f5943d14749c0b90851ec5824
Walid Chiakh <walidchiakh@MacBook-Air-de-Walid.local>
2023-05-17 11:51:18 +0200

    exercice final

---
+++ src/components/PokemonList/Pokemon.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+ export default function Pokemon({poke}){
+     const [pokemon, setPokemon] = useState({})
+     useEffect(()=>{
+         async function getPokemon(){
+             const response = await fetch(poke.url)
+             const data = await response.json()
+             setPokemon(data)
+         }
+         getPokemon()
+     },[])
+     return (
+             <li key={pokemon.name}> 
+             <img alt={pokemon.name} src={pokemon?.sprites?.front_default} /> 
+             {pokemon.name}
+             </li>
+         )
+ }
+
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,14 +1,31 @@
-import React  from "react";
-
+import React from "react";
+import Pokemon from './Pokemon'
+import { useEffect, useState } from "react";
+ 
 function PokemonList({ searchTerm = "" }) {
-
-
+  const [list_pokemons, setlist_Pokemon] = useState([])
+  useEffect(()=>{
+    async function loadlist_Pokemon(){
+       const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+        const data = await response.json()
+        setlist_Pokemon(data.results)
+      }
+    loadlist_Pokemon()
+  }, [])
+ 
+ 
   return (
     <ul>
-    <li>test</li>
-    <li>test2</li>
+    {
+      list_pokemons?.map((pokemon, index)=>{
+        if(pokemon.name.includes(searchTerm)){
+          return <Pokemon poke={pokemon} />
+        }
+        return null
+      })
+    }
     </ul>
   );
 }
-
+ 
 export default PokemonList;
